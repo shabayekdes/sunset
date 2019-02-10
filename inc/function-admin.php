c274dfb80c520c7745ae82bda5b9151a950da5bc
ESMAIL SHABAYEK <esmail.shabayek@gmail.com>
2019-02-10 20:18:31 +0200

    create Admin SubPages

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -11,12 +11,23 @@
 
 function sunset_add_admin_page()
 {
-    add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'shabayekdes_sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
+    //Generate Sunset Admin Page
+    add_menu_page( __( 'Sunset Theme Options', 'shabayekdes' ), 'Sunset', 'manage_options', 'shabayekdes_sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
+
+    //Generate Sunset Admin Sub Pages
+    add_submenu_page( 'shabayekdes_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'shabayekdes_sunset', 'sunset_theme_create_page' );
+    add_submenu_page( 'shabayekdes_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'shabayekdes_sunset_css', 'sunset_theme_settings_page');
+
 
 }
 add_action( 'admin_menu', 'sunset_add_admin_page' );
 
 function sunset_theme_create_page()
 {
-    //generation of our admin page
+    require_once( get_template_directory() . '/inc/templates/sunset-admin.php' );
+}
+function sunset_theme_settings_page()
+{
+    echo '<h1>Sunset Custom CSS</h1>';
+
 }
---
+++ inc/templates/sunset-admin.php
@@ -0,0 +1,5 @@
+<h1>Sunset Theme Options</h1>
+<h3 class="title">Manage Options</h3>
+<p>Customize the default Wordpress Appearance Options</p>
+
+<?php bloginfo('name'); ?>
