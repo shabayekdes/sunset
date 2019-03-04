c71e6df0bbfb86cdc9320509441889e9896fdb40
ESMAIL SHABAYEK <esmail.shabayek@gmail.com>
2019-03-04 23:16:49 +0200

    Create Video Post Format

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -96,8 +96,13 @@ function sunset_get_attachment(){
 }
 function sunset_get_embedded_media( $type = array() ){
 	$content = do_shortcode( apply_filters( 'the_content', get_the_content() ) );
-	$embed = get_media_embedded_in_content( $content );
-	// var_dump($embed);
-	$output = str_replace( '?visual=true', '?visual=false', $embed[0] );
+	$embed = get_media_embedded_in_content( $content, $type );
+	
+	if( in_array( 'audio' , $type) ):
+		$output = str_replace( '?visual=true', '?visual=false', $embed[0] );
+	else:
+		$output = $embed[0];
+	endif;
+	
 	return $output;
 }
