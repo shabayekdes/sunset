6773d2224d6b6625e74d4d95a4e1033c85e85d3b
ESMAIL SHABAYEK <esmail.shabayek@gmail.com>
2019-03-04 22:01:28 +0200

    Create Image Post Format

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -71,3 +71,26 @@ function sunset_posted_footer(){
 	
 	return '<div class="post-footer-container"><div class="row"><div class="col-xs-12 col-sm-6">'. get_the_tag_list('<div class="tags-list"><span class="sunset-icon sunset-tag"></span>', ' ', '</div>') .'</div><div class="col-xs-12 col-sm-6 text-right">'. $comments .'</div></div></div>';
 }
+function sunset_get_attachment(){
+	
+	$output = '';
+	if( has_post_thumbnail() ): 
+		$output = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
+	else:
+		$attachments = get_posts( array( 
+			'post_type' => 'attachment',
+			'posts_per_page' => 1,
+			'post_parent' => get_the_ID()
+		) );
+		if( $attachments ):
+			foreach ( $attachments as $attachment ):
+				$output = wp_get_attachment_url( $attachment->ID );
+			endforeach;
+		endif;
+		
+		wp_reset_postdata();
+		
+	endif;
+	
+	return $output;
+}
