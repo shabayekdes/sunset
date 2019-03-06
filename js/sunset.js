005f4985af7e22a7d266e256f03ff9ff1e66b28f
ESMAIL SHABAYEK <esmail.shabayek@gmail.com>
2019-03-06 17:51:57 +0200

    Bug Fix! Gallery Post Format

--- js/sunset.js
+++ js/sunset.js
@@ -10,10 +10,14 @@ jQuery(document).ready( function($){
 	});
 	
 	function sunset_get_bs_thumbs( carousel ){
-		var nextThumb = $('.item.active').find('.next-image-preview').data('image');
-		var prevThumb = $('.item.active').find('.prev-image-preview').data('image');
-		$(carousel).find('.carousel-control.right').find('.thumbnail-container').css({ 'background-image' : 'url('+nextThumb+')' });
-		$(carousel).find('.carousel-control.left').find('.thumbnail-container').css({ 'background-image' : 'url('+prevThumb+')' });
+		$(carousel).each(function(){
+			var nextThumb = $(this).find('.item.active').find('.next-image-preview').data('image');
+			var prevThumb = $(this).find('.item.active').find('.prev-image-preview').data('image');
+			$(this).find('.carousel-control.right').find('.thumbnail-container').css({ 'background-image' : 'url('+nextThumb+')' });
+			$(this).find('.carousel-control.left').find('.thumbnail-container').css({ 'background-image' : 'url('+prevThumb+')' });
+		});
+
+
 	}
 	
 });
