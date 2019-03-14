e50143abb81a613e6ac58db04fda0798cae58d06
ESMAIL SHABAYEK <esmail.shabayek@gmail.com>
2019-03-14 21:09:36 +0200

    Create Dynamic Archive Page

--- archive.php
+++ archive.php
@@ -7,10 +7,14 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			
+			<header class="archive-header text-center">
+				<?php the_archive_title('<h1 class="page-title">', '</h1>'); ?>
+			</header>
+			
 			<?php if( is_paged() ): ?>
 			
 				<div class="container text-center container-load-previous">
-					<a class="btn-sunset-load sunset-load-more" data-prev="1" data-page="<?php echo sunset_check_paged(1); ?>" data-url="<?php echo admin_url('admin-ajax.php'); ?>">
+					<a class="btn-sunset-load sunset-load-more" data-prev="1" data-archive="<?php echo $_SERVER["REQUEST_URI"]; ?>" data-page="<?php echo sunset_check_paged(1); ?>" data-url="<?php echo admin_url('admin-ajax.php'); ?>">
 						<span class="sunset-icon sunset-loading"></span>
 						<span class="text">Load Previous</span>
 					</a>
@@ -24,7 +28,7 @@ get_header(); ?>
 					
 					if( have_posts() ):
 						
-						echo '<div class="page-limit" data-page="/' . sunset_check_paged() . '">';
+						echo '<div class="page-limit" data-page="' . $_SERVER["REQUEST_URI"] . '">';
 						
 						while( have_posts() ): the_post();
 							
@@ -45,7 +49,7 @@ get_header(); ?>
 			</div><!-- .container -->
 			
 			<div class="container text-center">
-				<a class="btn-sunset-load sunset-load-more" data-page="<?php echo sunset_check_paged(1); ?>" data-url="<?php echo admin_url('admin-ajax.php'); ?>">
+				<a class="btn-sunset-load sunset-load-more" data-page="<?php echo sunset_check_paged(1); ?>" data-archive="<?php echo $_SERVER["REQUEST_URI"]; ?>" data-url="<?php echo admin_url('admin-ajax.php'); ?>">
 					<span class="sunset-icon sunset-loading"></span>
 					<span class="text">Load More</span>
 				</a>
--- inc/ajax.php
+++ inc/ajax.php
@@ -12,25 +12,69 @@ add_action( 'wp_ajax_sunset_load_more', 'sunset_load_more' );
 function sunset_load_more() {
 	
 	$paged = $_POST["page"]+1;
+	$prev = $_POST["prev"];
+	$archive = $_POST["archive"];
 	
-	$query = new WP_Query( array(
+	if( $prev == 1 && $_POST["page"] != 1 ){
+		$paged = $_POST["page"]-1;
+	}
+	
+	$args = array(
 		'post_type' => 'post',
 		'post_status' => 'publish',
 		'paged' => $paged
-	) );
+	);
+	
+	if( $archive != '0' ){
+		
+		$archVal = explode( '/', $archive );
+		
+		$type = ( $archVal[1] == "category" ? "category_name" : $archVal[1] );
+		
+		$args[ $type ] = $archVal[2];
+		
+		$page_trail = '/' . $archVal[1] . '/' . $archVal[2] . '/';
+		
+	} else {
+		$page_trail = '/';
+	}
+	
+	$query = new WP_Query( $args );
 	
 	if( $query->have_posts() ):
-						
+		
+		echo '<div class="page-limit" data-page="' . $page_trail . 'page/' . $paged . '">';
+				
 		while( $query->have_posts() ): $query->the_post();
 		
 			get_template_part( 'template-parts/content', get_post_format() );
 		
 		endwhile;
 		
+		echo '</div>';
+		
+	else:
+	
+		echo 0;
+		
 	endif;
 	
 	wp_reset_postdata();
 	
 	die();
+	
+}
+function sunset_check_paged( $num = null ){
+	
+	$output = '';
+	
+	if( is_paged() ){ $output = 'page/' . get_query_var( 'paged' ); }
+	
+	if( $num == 1 ){
+		$paged = ( get_query_var( 'paged' ) == 0 ? 1 : get_query_var( 'paged' ) );
+		return $paged;
+	} else {
+		return $output;
+	}
 	
 }
